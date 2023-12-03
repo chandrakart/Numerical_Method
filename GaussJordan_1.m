24c52f9223ffbacf9ef0e1c25649f766842005d7
Chandra <118231592+chandrakart@users.noreply.github.com>
2023-12-04 01:54:27 +0700

    Create GaussJordan_1.m

---
+++ GaussJordan_1.m
@@ -0,0 +1,38 @@
+% Matriks koefisien
+A = [2, 8, 6; 4, 2, -2; 3, -2, 1];
+
+% Matriks augmented
+B = [20; -2; 12];
+
+% Matriks augmented gabungan
+AB = [A, B];
+
+% Eliminasi Gauss
+[m, n] = size(AB);
+
+for k = 1:min(m, n - 1)
+    % Pivoting
+    [~, pivot_row] = max(abs(AB(k:m, k)));
+    pivot_row = pivot_row + k - 1;
+    AB([k, pivot_row], :) = AB([pivot_row, k], :);
+
+    % Normalisasi
+    pivot = AB(k, k);
+    AB(k, :) = AB(k, :) / pivot;
+
+    % Eliminasi
+    for i = k + 1:m
+        factor = AB(i, k) / AB(k, k);
+        AB(i, :) = AB(i, :) - factor * AB(k, :);
+    end
+end
+
+% Back Substitution
+x = zeros(n - 1, 1);
+
+for i = m:-1:1
+    x(i) = AB(i, end) - AB(i, i+1:n-1) * x(i+1:n-1);
+end
+
+disp('Akar dari sistem persamaan linear:');
+disp(x);
